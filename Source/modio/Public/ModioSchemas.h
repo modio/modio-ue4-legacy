c3fb6eae714a010c637630910fd834b86ffc2870
turupawn <ahmed.hn.43@gmail.com>
2019-01-25 17:37:42 -0600

    modfile schema added

--- Source/modio/Public/ModioSchemas.h
+++ Source/modio/Public/ModioSchemas.h
@@ -92,6 +92,42 @@ struct FModioDownload
 };
 
 USTRUCT(BlueprintType)
+struct FModioModfile
+{
+  GENERATED_BODY()
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 Id;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 ModId;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 VirusStats;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 VirusPositive;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 DateAdded;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 DateScanned;
+  // TODO use int 64 instead of 32
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  int32 Filesize;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FString Filename;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FString Version;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FString VirustotalHash;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FString Changelog;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FString MetadataBlob;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FModioFilehash Filehash;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "mod.io")
+  FModioDownload Download;
+};
+
+USTRUCT(BlueprintType)
 struct FModioMod
 {
   GENERATED_BODY()
@@ -220,6 +256,8 @@ extern void initializeFilehash(FModioFilehash &filehash, modio::Filehash modio_f
 extern void initializeFileHashC(FModioFilehash &filehash, ModioFilehash modio_filehash);
 extern void initializeDownload(FModioDownload &download, modio::Download modio_download);
 extern void initializeDownloadC(FModioDownload &download, ModioDownload modio_download);
+extern void initializeModfile(FModioModfile &modfile, modio::Modfile modio_modfile);
+extern void initializeModfileC(FModioModfile &modfile, ModioModfile modio_modfile);
 extern void initializeMod(FModioMod &mod, modio::Mod modio_mod);
 extern void initializeModC(FModioMod &mod, ModioMod modio_mod);
 extern void initializeInstalledModC(FModioInstalledMod &installed_mod, ModioInstalledMod modio_installed_mod);
