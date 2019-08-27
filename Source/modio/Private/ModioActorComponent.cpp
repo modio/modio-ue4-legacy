5eb325badac3d0436445915f15600cbfac850bd3
turupawn <ahmed.hn.43@gmail.com>
2019-08-26 19:07:37 -0600

    added ModioActorComponent

---
+++ Source/modio/Private/ModioActorComponent.cpp
@@ -0,0 +1,17 @@
+// Copyright 2019 modio. All Rights Reserved.
+// Released under MIT.
+
+#include "ModioActorComponent.h"
+
+UModioActorComponent::UModioActorComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bStartWithTickEnabled = true;
+
+    this->Modio = FModioSubsystem::Get(GetWorld());
+}
+
+void UModioActorComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Modio->Process();
+}
---
+++ Source/modio/Public/ModioActorComponent.h
@@ -0,0 +1,18 @@
+// Copyright 2019 modio. All Rights Reserved.
+// Released under MIT.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "ModioSubsystem.h"
+#include "ModioActorComponent.generated.h"
+
+UCLASS(meta = (BlueprintSpawnableComponent), DisplayName = "mod.io Actor Component")
+class MODIO_API UModioActorComponent : public UActorComponent
+{
+  GENERATED_UCLASS_BODY()
+
+  FModioSubsystemPtr Modio;
+
+  void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+};
